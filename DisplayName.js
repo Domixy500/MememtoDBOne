b2ee36fd2522f73d15365662e282bf143aa6720f
Domi <Dominik@PC2013>
2022-04-24 16:16:20 +0200

    Update DisplayName.js

--- DisplayName.js
+++ DisplayName.js
@@ -1,11 +1,20 @@
 var BaseType = field("BasisTyp");
 var DisplayName;
 switch (BaseType) {
+	case 'Aktivität':
+		DisplayName = formatDate(field("Datum"));
+		break;
 	case 'Kauf':
 		DisplayName = formatDate(field("Datum"));
 		break;
+	case 'Laufen':
+		DisplayName = field("Streckenlänge") + " km " + formatDate(field("Datum"));
+		break;
 	case 'Person':
 		DisplayName = field("Vorname") + " " + field("Nachname");
+		break;
+	case 'Radfahren':
+		DisplayName = field("Streckenlänge") + " km " + formatDate(field("Datum"));
 		break;
 	default:
 		DisplayName = field("Name");
