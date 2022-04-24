eb5d324bfc0f5d8e893588edde7e0bf00b18754d
Domi <Dominik@PC2013>
2022-04-24 16:02:43 +0200

    DisplayName without functions
    Just as BackUp

--- DisplayName.js
+++ DisplayName.js
@@ -1,16 +1,14 @@
-function getDisplayName(BaseType) {
-	var DisplayName;
-	switch (BaseType) {
-		case "Kauf":
-			DisplayName = formatDate(field("Datum"))
-			break;
-		case "Person":
-			DisplayName = field("Vorname") + " " + field("Nachname")
-			break;
-		default:
-			DisplayName = field("Name") 
-			break;
-	}
-	DisplayName = DisplayName + " [" + field("Id") + "]";
-	return DisplayName;
+var BaseType = field("BasisTyp");
+var DisplayName;
+switch (BaseType) {
+	case 'Kauf':
+		DisplayName = formatDate(field("Datum"));
+		break;
+	case 'Person':
+		DisplayName = field("Vorname") + " " + field("Nachname");
+		break;
+	default:
+		DisplayName = field("Name");
+		break;
 }
+DisplayName = DisplayName + " [" + field("Id") + "]";
